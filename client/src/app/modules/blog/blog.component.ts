4f7ad73a7c8c383781d998d670e7474091cad8d1
Alex <alex@alexseifert.com>
2024-03-19 21:57:04 +0100

    Add the page title component

--- client/src/app/modules/blog/blog.component.ts
+++ client/src/app/modules/blog/blog.component.ts
@@ -2,11 +2,13 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 
 import { WordPressPost } from '../../shared/types/blog.interface';
+import { PageTitleComponent } from '../../shared/components/page-title/page-title.component';
 
 @Component({
   selector: 'app-blog',
   standalone: true,
   templateUrl: './blog.component.html',
+  imports: [PageTitleComponent],
   styleUrl: './blog.component.scss',
 })
 export class BlogComponent implements OnInit {
