2dfeb6b2c733fbf86fcd08780ee801397bf487cb
Alex Seifert <alex@alexseifert.com>
2024-03-19 09:45:07 +0100

    Add the blog module

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -5,4 +5,8 @@ export const routes: Routes = [
     path: '',
     loadChildren: () => import('./modules/home/home.module').then(m => m.HomeModule),
   },
+  {
+    path: 'blog',
+    loadChildren: () => import('./modules/blog/blog.module').then(m => m.BlogModule),
+  },
 ];
