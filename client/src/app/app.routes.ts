5e75fdb2a040b02c71b5fafe6063eec6eb0346af
Alex Seifert <alex@alexseifert.com>
2024-03-19 09:06:38 +0100

    Add and fix linting

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,8 +1,8 @@
-import { Routes } from '@angular/router';
+import { Routes, } from '@angular/router';
 
 export const routes: Routes = [
   {
     path: '',
-    loadChildren: () => import('./modules/home/home.module').then(m => m.HomeModule)
-  }
+    loadChildren: () => import('./modules/home/home.module').then(m => m.HomeModule),
+  },
 ];
--- client/src/app/modules/home/home-routing.module.ts
+++ client/src/app/modules/home/home-routing.module.ts
@@ -1,18 +1,18 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { NgModule, } from '@angular/core';
+import { RouterModule, Routes, } from '@angular/router';
 
-import { HomeComponent } from './home.component';
+import { HomeComponent, } from './home.component';
 
 const routes: Routes = [
   {
     path: '',
     title: 'Home',
-    component: HomeComponent
+    component: HomeComponent,
   },
 ];
 
 @NgModule({
-  imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  imports: [RouterModule.forChild(routes),],
+  exports: [RouterModule,],
 })
 export class HomeRoutingModule { }
