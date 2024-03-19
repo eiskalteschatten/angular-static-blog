3a6c7527d50cb043a5b6bd29f2223885ec0ed838
Alex Seifert <alex@alexseifert.com>
2024-03-19 09:56:11 +0100

    Add the active state to the main nav

--- client/src/app/static/main-nav/main-nav.component.ts
+++ client/src/app/static/main-nav/main-nav.component.ts
@@ -1,13 +1,24 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { NavigationEnd, Router, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-main-nav',
   standalone: true,
-  imports: [RouterModule],
+  imports: [CommonModule, RouterModule],
   templateUrl: './main-nav.component.html',
   styleUrl: './main-nav.component.scss'
 })
 export class MainNavComponent {
+  currentRoute = '/';
 
+  constructor(
+    private router: Router
+  ) {
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        this.currentRoute = event.url;
+      }
+    });
+  }
 }
