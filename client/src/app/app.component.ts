f6c049bfdf14f3ba16d91b83d0a4c9343c144e0a
Alex Seifert <alex@alexseifert.com>
2024-03-19 10:11:36 +0100

    Add the route loader

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 import { HeaderComponent } from './static/header/header.component';
+import { RouteLoaderComponent } from './static/route-loader/route-loader.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [RouterOutlet, HeaderComponent, RouteLoaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
---
+++ client/src/app/static/route-loader/route-loader.component.ts
@@ -0,0 +1,51 @@
+import { CommonModule, isPlatformBrowser } from '@angular/common';
+import { Component, Inject, PLATFORM_ID } from '@angular/core';
+import { NavigationEnd, NavigationStart, Router } from '@angular/router';
+
+@Component({
+  selector: 'app-route-loader',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './route-loader.component.html',
+  styleUrl: './route-loader.component.scss'
+})
+export class RouteLoaderComponent {
+  pageIsLoading = false;
+  barWidth = 0;
+
+  private widthInterval?: NodeJS.Timeout;
+
+  constructor(
+    private router: Router,
+    @Inject(PLATFORM_ID) private platformId: Object
+  ) {
+    this.router.events.subscribe((event) => {
+      if (isPlatformBrowser(this.platformId)) {
+        if (event instanceof NavigationStart) {
+          console.log('navigation start');
+          if (this.widthInterval) {
+            clearInterval(this.widthInterval);
+          }
+
+          this.pageIsLoading = true;
+
+          this.widthInterval = setInterval(() => {
+            this.barWidth = this.barWidth + 50;
+
+            if (this.barWidth > screen.width) {
+              clearInterval(this.widthInterval);
+            }
+          }, 1000);
+        }
+        else if (event instanceof NavigationEnd) {
+          this.pageIsLoading = false;
+          this.barWidth = 0;
+
+          if (this.widthInterval) {
+            clearInterval(this.widthInterval);
+          }
+        }
+      }
+    });
+  }
+}
