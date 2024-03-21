fc7ed57dcce4197a82f3da86ca1ffacbf4460490
Alex Seifert <alex@alexseifert.com>
2024-03-21 09:38:47 +0100

    Fix the @enivronment alias

--- client/src/app/directusClient.ts
+++ client/src/app/directusClient.ts
@@ -1,4 +1,4 @@
 import { createDirectus, graphql } from '@directus/sdk';
-import environment from '@environment';
+import { environment } from '@environment';
 
 export const directusClient = createDirectus(environment.directus.url).with(graphql());
