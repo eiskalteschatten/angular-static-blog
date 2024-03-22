1360861ab42cd02b4022a1c8047010af3c746c0e
Alex Seifert <alex@alexseifert.com>
2024-03-22 11:41:20 +0100

    Update the blog post interfaces for the post blocks

--- client/src/app/shared/types/blog.interface.ts
+++ client/src/app/shared/types/blog.interface.ts
@@ -2,7 +2,7 @@ export interface BlogPost {
   id: number;
   title: string;
   slug: string;
-  post: string;
+  post: BlogPostBlock[];
   excerpt?: string;
   featuredImage?: string;
   'date_published': string;
@@ -17,4 +17,45 @@ export interface BlogPost {
 export interface BlogCategory {
   name: string;
   slug: string;
+}
+
+export interface BlogPostBlock {
+  id: string;
+  type: string;
+  data: {
+    text?: string;
+    code?: string;
+    level?: number;
+    file?: BlogPostFile;
+    caption?: string;
+    withBorder?: boolean;
+    stretched?: boolean;
+    withBackground?: boolean;
+    alignment?: string;
+    style?: string;
+    items?: BlogCheckListItem[];
+  };
+}
+
+interface BlogPostFile {
+  width: number;
+  height: number;
+  size: number;
+  name: string;
+  title: string;
+  extension: string;
+  fileId: string;
+  fileURL: string;
+  url: string;
+}
+
+interface BlogCheckListItem {
+  content?: string;
+  items?: {
+    content?: string;
+    text?: string;
+    checked?: boolean;
+  }[];
+  text?: string;
+  checked?: boolean;
 }
