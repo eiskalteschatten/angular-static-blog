99750ae2e2cd480235395d1f545188e3a7e33774
Alex Seifert <alex@alexseifert.com>
2024-03-24 12:43:19 +0100

    Get the project running again

--- src/app/directusClient.ts
+++
@@ -1,4 +0,0 @@
-import { createDirectus, graphql } from '@directus/sdk';
-import { environment } from '@environment';
-
-export const directusClient = createDirectus(environment.directus.url).with(graphql());
--- src/app/shared/services/blog.service.ts
+++ src/app/shared/services/blog.service.ts
@@ -1,85 +1,25 @@
 import { Injectable } from '@angular/core';
 
-import { directusClient } from '@/directusClient';
-
 import { BlogCategory, BlogPost } from '../types/blog.interface';
 
-
-interface BlogPostsSchema {
-  blogPosts: BlogPost[];
-}
-
-interface BlogPostSchema {
-  blogPosts: BlogPost[];
-}
-
-interface BlogCategorySchema {
-  categories: BlogCategory[];
-}
 
 @Injectable({
   providedIn: 'root',
 })
 export class BlogService {
   async getAllPosts(): Promise<BlogPost[]> {
-    const { blogPosts } = await directusClient.query<BlogPostsSchema>(`
-      query GetBlogPosts {
-        blogPosts(sort: ["-date_published"], filter: { status: { _eq: "published" } }) {
-          title
-          slug
-          excerpt
-          featuredImage
-          date_published
-          user_created
-          categories {
-            categories_id {
-              name
-              slug
-            }
-          }
-        }
-      }
-    `);
-
+    const blogPosts: BlogPost[] = [];
     return blogPosts;
   }
 
   async getSinglePost(slug: string): Promise<BlogPost | undefined> {
-    const { blogPosts } = await directusClient.query<BlogPostSchema>(`
-      query GetBlogPost {
-        blogPosts(filter: { slug: { _eq: "${slug}" } }) {
-          title
-          slug
-          post
-          excerpt
-          featuredImage
-          date_published
-          user_created
-          user_updated
-          tags
-          categories {
-            categories_id {
-              name
-              slug
-            }
-          }
-        }
-      }
-    `);
-
-    return blogPosts[0];
+    console.log('slug', slug);
+    return undefined;
+    // return blogPost;
   }
 
   async getAllCategories(): Promise<BlogCategory[]> {
-    const { categories } = await directusClient.query<BlogCategorySchema>(`
-      query GetBlogCategories {
-        categories {
-          name
-          slug
-        }
-      }
-    `);
-
+    const categories: BlogCategory[] = [];
     return categories;
   }
 }
