84d8c988c63d09d93ee5c8d52859a312dfacb61c
Alex Seifert <alex@alexseifert.com>
2024-04-12 13:27:43 +0200

    Cleanup

--- src/app/shared/services/blog.service.ts
+++ src/app/shared/services/blog.service.ts
@@ -3,7 +3,6 @@ import { Injectable } from '@angular/core';
 import { BlogPostMetaData, BlogCategory, BlogPost } from '@/shared/types/blog.interface';
 import blogPostIds from '@/data/blog/posts';
 
-
 @Injectable({
   providedIn: 'root',
 })
